7b805fc8218d482a03b78a9ff04c6c7309bcf93d
ClaytonMoutzouris <clayton_moutzouris@hotmail.com>
2017-03-28 18:25:53 -0400

    Added Background Music
    Downloaded a free .wav file from the web, and I think it suits the game
    right now. However, the Wav file is 8mb, so it might be taken out for
    the next version. For now it was a nice little exercise is handling
    sound.

---
+++ OTiS/Assets/SoundControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class SoundControllerScript : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioClip backgroundMusic;
+    public static SoundControllerScript instance = null;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+           // DontDestroyOnLoad(gameObject);
+        }
+        gameObject.AddComponent<AudioSource>();
+        musicSource = gameObject.GetComponent<AudioSource>();
+        musicSource.clip = backgroundMusic;
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(musicSource.isPlaying))
+        {
+            musicSource.Play();
+        }
+        else
+        {
+            //Debug.log("Something is wrong with Music.");
+        }
+    }
+
+
+}
